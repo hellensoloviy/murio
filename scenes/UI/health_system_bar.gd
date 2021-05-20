1bf05211e031a6350ba2835348ebe1baf93e17b5
Hellen <hellensoloviy@gmail.com>
2021-05-20 22:31:44 +0300

    MRO-23 update UI for health bar
    now it shows empty hearts; 
    add death signal definition;
    put default health value at max;

--- scenes/UI/health_system_bar.gd
+++ scenes/UI/health_system_bar.gd
@@ -3,7 +3,6 @@ extends Control
 var heart_full = preload("res://sprites/GUI/hudHeart_full.png")
 var heart_empty = preload("res://sprites/GUI/hudHeart_empty.png")
 
-
 export var current_value = 5
 export var max_value = 5
 export var step = 1
@@ -13,10 +12,17 @@ func _ready():
 	pass 
 
 func update_heart_value(value):
+	if value == 0: 
+		death_event()
+	
 	for i in $bar.get_child_count():
 		if i < value:
-			$bar.get_child(i).show()
+			$bar.get_child(i).texture = heart_full
 		else: 
-			$bar.get_child(i).hide()
+			$bar.get_child(i).texture = heart_empty
 		
+	pass
+
+func death_event():
+	#death event will be here
 	pass
