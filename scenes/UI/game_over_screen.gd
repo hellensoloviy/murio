0569c194d3d63df1f6f22e9ef7fcab42bc6a7841
Hellen <hellensoloviy@gmail.com>
2021-05-25 21:40:18 +0300

    MRO-24, add game over screen (v1);

--- scenes/UI/game_over_screen.gd
+++ scenes/UI/game_over_screen.gd
@@ -1,9 +1,16 @@
 extends Control
 
 const menu_screen_path = "res://scenes/UI/Menu.tscn"
+const start_again_path = "res://MainScene.tscn"
 
 func _ready():
 	pass 
+	
 
-func _on_go_to_title_button_pressed():
+func _on_go_to_title_screen_pressed():
 	get_tree().change_scene(menu_screen_path)
+
+
+func _on_load_game_button_pressed():
+	#TODO: - 
+	get_tree().change_scene(start_again_path)
